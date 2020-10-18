67a55d1f5888dbf9611c9121910d4583e7075b59
Luis <luisedumlg@gmail.com>
2020-10-18 04:54:51 +0000

    Versao inicial do cliente

--- client.cpp
+++ client.cpp
@@ -1,29 +1,32 @@
-#include <HTTPReq.hpp>
-#include <HTTPRes.hpp>
+#include "HTTPReq.hpp"
+#include "HTTPRes.hpp"
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 #include <string.h>
 #include <stdio.h>
-#include <errno.h>
+#include <iostream>
 #include <unistd.h>
+#include <sstream>
 
 //Função que retorna o endereço de um dado hostname
-std::string getAddress(std::string hostname, std::string port){
+std::string getAddress(std::string hostname){
 	
 	struct addrinfo hints;
 	struct addrinfo* res;
 
-	std::string ipString
+	std::string ipString;
+	std::string UM;
 
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 
-	int status = getaddrinfo(hostname,"80",&hints,&res)
+	int status = getaddrinfo(hostname.c_str(),"80",&hints,&res);
 	if(status != 0){
 		std::cerr << "ERRO: hostname nao encontrado" << std::endl;
-		return 1;
+		return UM;
 	}
 
 	for(struct addrinfo* p = res; p != 0; p = p->ai_next) {
@@ -32,7 +35,7 @@ std::string getAddress(std::string hostname, std::string port){
 	    
 	    char ipstr[INET_ADDRSTRLEN] = {'\0'};
 	    inet_ntop(p->ai_family, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
-	    ipString = ipstr
+	    ipString = ipstr;
 	}
 	
 	freeaddrinfo(res); 
@@ -43,33 +46,35 @@ std::string serverRequest(std::string address, std::string port, std::string req
 	
 	//Definindo o socket que foi passado como argumento 
 	int socketfd = socket(AF_INET,SOCK_STREAM,0);
-	portInt = std::stoi(port);
+	int portInt = std::stoi(port);
 
 	struct sockaddr_in serverAddr;
 
 	//Conectando o socket do servidor
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_port = htons(portInt);
-	serverAddr.sin_addr.s_addr = inet_addr(address);
+	serverAddr.sin_addr.s_addr = inet_addr(address.c_str());
 	memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
 
-	//Testando se a conexão é bem sucetível
+	std::string UM = "1";
+
+	//Testando se a conexão é bem sucedida
 	if (connect(socketfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
 		std::cerr << "ERRO: Nao foi possivel conectar no socket" << std::endl;
-	    	return 1;
+	    	return UM;
 	}
 	
 	
 	//Enviando o request para o cliente
 	if(send(socketfd,request.c_str(),request.size(),0) == -1){
 		std::cerr << "ERRO: Nao foi possivel enviar o request ao socket" << std::endl;
-		return 1;
+		return UM;
 	}
 
 	//Recebendo o request do cliente
 	std::string response;
-	std::stringstream ss;
-	char buffer[20] = {0}
+	std::stringstream responseStream;
+	char buffer[20] = {0};
 	bool receivedMessage = false;
 
 	//Recebendo mensagem 
@@ -77,20 +82,30 @@ std::string serverRequest(std::string address, std::string port, std::string req
 	
 		memset(buffer,'\0',sizeof(buffer));
 
-		if(recv(socketfd,bu,20,0)==-1){
-			std::cerr << "ERRO: Nao conseguimos receber a mensagem do servidor";
-			return 1;
+		//Tentando receber a mensagem do servidor
+		if(recv(socketfd,buffer,20,0)==-1){
+			std::cerr << "ERRO: Nao recebi a mensagem do servidor" << std::endl;
+			return UM;
 		}
 
+		//Pega o conteudo que foi recebido e colocando na resposta
+		responseStream << buffer;
+		response = buffer;
+
+		if(response.length() == 0)
+			break;
 	}
 
+	response = responseStream.str();
+	return response;
 }
 
 int main (int argc, char* argv[]){
 	//Definindo a string para ler os argumnetos
-	std::string requestUrl 
-	std::string HTTP = "http://"
-	HTTPReq request
+	std::string UM = "1";
+	std::string requestUrl; 
+	std::string HTTP = "http://";
+	HTTPReq request;
 	//Vamos checar se algum argumento foi passado como argumento
 	//Se nenhum argumento foi passado, vamos retornar erro
 
@@ -105,28 +120,32 @@ int main (int argc, char* argv[]){
 
 		//Vamos checar se o request começa com a tag http
 		std::string tagInicial = requestUrl.substr(0,6);
-		if(not(tagIncial == HTTP)){
-			std:cerr << "ERRO: O request nao comeca com a tag http://" << std::endl;
+		if(not(tagInicial == HTTP)){
+			std::cerr << "ERRO: O request nao comeca com a tag http://" << std::endl;
 			return 1;
 		}
 		
 		//Se comeca com a tag HTTP, vamos pegar o hostname (valor da string até o :)
-		int j
+		int j;
 		std::string hostname = "";
-		for(j = 7; requestUrl[j] != ":"; j++)
-			hostname.append(requestUrl[j]);
+		for(j = 7; requestUrl[j] != ':'; j++)
+			hostname+= requestUrl[j];
 		
 		//Agora, vamos pegar o valor da porta
 		std::string port = "";
-		for(;requestUrl[j] != "/";j++)
-			port.append(requestUrl[j]);
+		for(;requestUrl[j] != '/';j++)
+			port+=requestUrl[j];
 
 		//O final da url é o arquivo que temos que fazer a requisição para o servidor
 		std::string file = hostname.substr(j,requestUrl.length()-1);
 
 		//Achando o endereço IP do hostname
 		std::string addressString = getAddress(hostname);
-	
+		if(addressString == UM)
+			return 1;
+
+
+		
 		//Vamos setar os parametros do request que será feito
 		request.setMethod("URL");
 		request.setURL(requestUrl);
@@ -136,11 +155,14 @@ int main (int argc, char* argv[]){
 		//Achando a string que, dados os parametros, faz o request
 		std::string requestString = request.makeRequest();
 
-
 		//Realizando o request
 		std::string response = serverRequest(addressString,port,requestString);
+		if(response == UM)
+			return 1;
 
+		std::cout << response;
 
+		return 0;
 
 	}
 }
